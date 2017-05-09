37bcc2ec1d33ab7c65a5b8b5a1b7d0b67cf90910
dam102 <dam102@SAGRES04.olimpia.ga>
2017-05-09 11:25:08 +0200

    clase xeometria

--- boletin17-Junit/src/Xeometria.java
+++ boletin17-Junit/src/Xeometria.java
@@ -1,4 +1,30 @@
 
 public class Xeometria {
-
+	int perimetroCadrado (int lado) {
+		if (lado<0) {
+			return -1;
+		}
+		return (lado * lado);
+	}
+	
+	int areaCadrado (int lado) {
+		if (lado<0){
+			return -1;
+		}	
+		return (lado * 4);
+	}
+	
+	double teoremaPitagoras (int catetoA, int catetoB) {
+		if (catetoA<=0 || catetoB<=0) {
+			return -1;
+		}
+		return Math.sqrt(catetoA*catetoA)  + (catetoB * catetoB);
+	}
+	
+	double teoremadePitagoras (double hipotenusa,double catetoA){
+		if (catetoA<=0 || hipotenusa <=0) {
+			return -1;
+		}
+		return Math.sqrt(catetoA*catetoA)  + (hipotenusa * hipotenusa);
+	}
 }
